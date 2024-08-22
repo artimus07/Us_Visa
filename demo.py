bce18042b410d2790651b95e59290b275eb13263
Artimus <rajbhongde4@gmail.com>
2024-08-22 16:40:03 +0530

    Data Ingestion Done

--- demo.py
+++ demo.py
@@ -1,7 +1,4 @@
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-try :
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e, sys)
+obj = TrainPipeline()
+obj.run_pipeline()
